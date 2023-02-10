c958b4dd41607ee9804b1de7676a7ea7aaef9be3
AEGELIS <80143824+AEGELIS@users.noreply.github.com>
2023-02-10 14:58:42 -0600

    Guardar proyecto

---
+++ src/main.js
@@ -0,0 +1,46 @@
+let main = document.getElementById('main');
+let logo = document.getElementById('logo');
+
+
+
+
+logo.setAttribute("src", 'img/img_index/alfred-logo-white.svg');
+
+let nav = document.getElementById("nav")
+let menu = document.getElementById("menu")
+function mostrasScroll() {
+    let scrolltop = document.documentElement.scrollTop
+    let alturaAnimado = nav.offsetTop
+    if (alturaAnimado + 150 < scrolltop) {
+        nav.classList.add('bg-white')
+        nav.classList.add('transition')
+        nav.classList.add('duration-400')
+        menu.classList.add('text-black')
+        logo.setAttribute("src", 'img/img_index/alfred-logo.svg');
+    }
+}
+
+function ocultarScroll() {
+    let scrolltop = document.documentElement.scrollTop
+    let alturaAnimado = nav.offsetTop
+    if (alturaAnimado < scrolltop) {
+        nav.classList.remove('bg-white')
+        menu.classList.remove('text-black')
+        menu.classList.add('text-white')
+        logo.setAttribute("src", 'img/img_index/alfred-logo-white.svg');
+    }
+}
+
+window.addEventListener('scroll', ocultarScroll)
+window.addEventListener('scroll', mostrasScroll)
+
+
+
+
+
+main.style.backgroundImage = 'url(img/img_index/header_img_2.jpg)'
+main.style.backgroundRepeat= "no-repeat";
+main.style.backgroundPosition= "center";
+main.style.backgroundSize= "cover";
+    
+
